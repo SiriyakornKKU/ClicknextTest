df35c1ded4d2ed4eafa5c46f9e87e77be0f51854
Friend Siriyakorn <friend.siriyakorn@gmail.com>
2022-08-21 22:00:13 +0700

    add images

---
+++ src/components/Layout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="layout">
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line
+  name: 'Layout',
+  props: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#layout {
+}
+</style>
